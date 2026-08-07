6566c0d0fc9728513f9dae9cb9b64bbce26ff6a5
Aidan <dev@aidanlowson.com>
2026-08-07 23:38:09 +0100

    Initial commit

---
+++ TCSA.OOP.LibraryManagementSystem/Program.cs
@@ -0,0 +1,7 @@
+﻿// string[] menuChoice = ["View Books", "Add Book", "Delete Book"];
+var menuChoices = new string[3] { "View Books", "Add Book", "Delete Book" };
+
+var choice = AnsiConsole.Prompt(
+        new SelectionPrompt&lt;string&gt;()
+        .Title("What do you want to do next?")
+        .AddChoices(menuChoices));
