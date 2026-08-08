7494ce04d4ccdbc0592fd724dd7771706f28fa4c
AidanLDev <dev@aidanlowson.com>
2026-08-08 09:28:09 +0100

    Maths game

--- TCSA.OOP.LibraryManagementSystem/Program.cs
+++ TCSA.OOP.LibraryManagementSystem/Program.cs
@@ -1,7 +1,7 @@
-﻿// string[] menuChoice = ["View Books", "Add Book", "Delete Book"];
-var menuChoices = new string[3] { "View Books", "Add Book", "Delete Book" };
+﻿using Spectre.Console;
+string[] menuChoices = ["View Books", "Add Book", "Delete Book"];
 
 var choice = AnsiConsole.Prompt(
-        new SelectionPrompt&lt;string&gt;()
+        new SelectionPrompt<string>()
         .Title("What do you want to do next?")
         .AddChoices(menuChoices));
