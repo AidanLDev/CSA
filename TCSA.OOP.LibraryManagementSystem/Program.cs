ed717e9399202207196a58f20c762502f7b97fc6
AidanLDev <dev@aidanlowson.com>
2026-08-10 08:55:49 +0100

    Use class

--- TCSA.OOP.LibraryManagementSystem/Program.cs
+++ TCSA.OOP.LibraryManagementSystem/Program.cs
@@ -1,12 +1,5 @@
-﻿using Spectre.Console;
-string[] menuChoices = ["View Books", "Add Book", "Delete Book", "Exit"];
-List<string> books = ["The Great Gatsby", "To Kill a Mockingbird", "1984", "Pride and Prejudice", "The Catcher in the Rye", "The Hobbit", "Moby-Dick", "War and Peace", "The Odyssey", "The Lord of the Rings", "Jane Eyre", "Animal Farm", "Brave New World", "The Chronicles of Narnia", "The Diary of a Young Girl", "The Alchemist", "Wuthering Heights", "Fahrenheit 451", "Catch-22", "The Hitchhiker's Guide to the Galaxy"];
-
-void continuePrompt()
-{
-        AnsiConsole.MarkupLine("Press Any Key to Continue.");
-        Console.ReadKey();
-}
+﻿using TCSA.OOP.LibraryManagementSystem;
+using Spectre.Console;
 
 bool viewingLibrary = true;
 
@@ -22,52 +15,13 @@ while (viewingLibrary)
         switch (choice)
         {
                 case MenuOption.ViewBooks:
-                        AnsiConsole.MarkupLine("[yellow]List of books:[/]");
-                        foreach (string book in books)
-                        {
-                                AnsiConsole.MarkupLine($"- [cyan]{book}[/]");
-                        }
-
-                        continuePrompt();
-
+                        BooksController.ViewBooks();
                         break;
                 case MenuOption.AddBook:
-                        var title = AnsiConsole.Ask<string>("Enter the [green]title[/] of the book to add");
-
-                        if (books.Contains(title))
-                        {
-                                AnsiConsole.MarkupLine($"Library already has [red]{title}[/]");
-                        }
-                        else
-                        {
-                                books.Add(title);
-                        }
-
-                        continuePrompt();
+                        BooksController.AddBook();
                         break;
                 case MenuOption.DeleteBook:
-                        if (books.Count == 0)
-                        {
-                                AnsiConsole.MarkupLine("[red]No books to delete, library is empty[/]");
-                                Console.ReadLine();
-                                return;
-                        }
-
-                        string bookToDelete = AnsiConsole.Prompt(
-                                new SelectionPrompt<string>()
-                                .Title("Select a [red]book[/] to delete")
-                                .AddChoices(books)
-                        );
-
-                        if (books.Remove(bookToDelete))
-                        {
-                                AnsiConsole.MarkupLine($"[green]{bookToDelete} was successfully removed[/]");
-                        }
-                        else
-                        {
-                                AnsiConsole.MarkupLine("[red]Book not found[/]");
-                        }
-                        continuePrompt();
+                        BooksController.DeleteBook();
                         break;
                 case MenuOption.Exit:
                         AnsiConsole.MarkupLine("Thanks for visiting our library");
